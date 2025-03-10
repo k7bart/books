48e88c35f1f007edd1cef7d5f48e4d4c2fe8e7d1
Kateryna Bartienieva <k7bart@gmail.com>
2025-03-10 09:42:17 +0200

    add BooksContext, add hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import routes from "./AppRouter";
+import { BooksProvider } from "./context/books";
+
 import "./index.scss";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BooksProvider>
+      <RouterProvider router={router} />
+    </BooksProvider>
   </StrictMode>
 );
